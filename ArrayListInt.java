8fda6e793fce52c3a859c49ca5f2ef0ce17b7dd8
eduardo <eduardocardomartinez@gmail.com>
2015-02-20 19:03:42 +0100

    Añadidos metodos add con elemento por paramentro y add con elemento por paramentro e indice

---
+++ ArrayListInt.java
@@ -0,0 +1,72 @@
+
+/**
+ * Write a description of class ArrayListInt here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ArrayListInt
+{
+    //array que almacena una coleccion de numeros enteros
+    private int [] arrayList ;
+
+    /**
+     * Constructor for objects of class ArrayListInt
+     */
+    public ArrayListInt()
+    {
+        arrayList = new int[0];   //se crea un array de tamaño 0 que luego se redimensionara
+    }
+
+    
+    /**
+    /**
+     * Metodo que añade un elemento a la coleccion al final de la misma
+     */
+    public void add(int elemento)
+    {
+
+        //creamos un array temporal con un tamaño igual al del array inicial aumentado en 1
+        int[] temp = new int[arrayList.length + 1];
+        //almacenamos el elemento pasado por paramentro en la ultima posicion
+        temp[temp.length-1] = elemento; 
+        //almacenamos los objetos existentes en el array original en el temporal
+        for(int i=0; i<arrayList.length;i++)
+        {
+            temp[i] = arrayList[i];
+        }
+        arrayList=temp;
+    }
+
+    /**
+     * Metodo que añade un elemento a la coleccion en la posicion especificada por parametro
+     * si no existe ese indice no hace nada
+     */
+    public void add(int index,int elemento)
+    {
+        if((index<=arrayList.length) && (index>=0))    //si el indice pasado por parametro es un valor adecuado
+        {
+            //creamos un array temporal con un tamaño igual al del array inicial aumentado en 1
+            int[] temp = new int[arrayList.length + 1];
+            int i = 0;      //contador para indicar la posicion de los objetos en el array original
+            for(int cont= 0; cont< temp.length; cont++)    //se repite el bucle mientras no se supere el tamaño de la coleccion
+            {
+
+                if(cont== index)   //si el indice pasado por parametro coincide con alguno existente en la coleccion
+                {
+                    temp[cont] = elemento;    //almacenamos el objeto en esa posicion
+
+                }
+                else  //sino coincide almacenamos el objeto del array original en el array temporal 
+                {
+                    temp[cont] = arrayList[i]; 
+                    i++;     //aumentamos el indice del array original
+                }
+                
+            }
+            arrayList = temp;
+        }
+    }
+
+   
+}
